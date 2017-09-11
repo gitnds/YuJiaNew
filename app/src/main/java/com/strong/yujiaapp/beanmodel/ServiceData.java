d6b8ff92e800cc14e18c9cc22d1dacf7bac51f3d
haiyun <123>
2017-09-11 15:29:12 +0800

    haiyun

---
+++ app/src/main/java/com/strong/yujiaapp/beanmodel/ServiceData.java
@@ -0,0 +1,26 @@
+package com.strong.yujiaapp.beanmodel;
+
+/**
+ * Created by Administrator on 2017-09-11.
+ */
+
+public class ServiceData {
+    private int imageId;
+    private String message;
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
